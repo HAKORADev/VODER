123b1acd5a67a501752a2f05fa75ef1b290f4055
HAKORA <162914764+HAKORADev@users.noreply.github.com>
2026-02-10 23:19:31 +0200

    Adjust freqs_cis handling in apply_rotary_emb function

--- src/modules/astral_quantization/transformer.py
+++ src/modules/astral_quantization/transformer.py
@@ -240,7 +240,8 @@ def precompute_freqs_cis(
 
 def apply_rotary_emb(x: Tensor, freqs_cis: Tensor) -> Tensor:
     xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
-    freqs_cis = freqs_cis.view(1, x.size(1), 1, x.size(3) // 2, 2)
+    seq_len = x.size(1)
+    freqs_cis = freqs_cis[:seq_len].view(1, seq_len, 1, x.size(3) // 2, 2)
     x_out2 = torch.stack(
         [
             xshaped[..., 0] * freqs_cis[..., 0] - xshaped[..., 1] * freqs_cis[..., 1],
@@ -251,4 +252,3 @@ def apply_rotary_emb(x: Tensor, freqs_cis: Tensor) -> Tensor:
 
     x_out2 = x_out2.flatten(3)
     return x_out2.type_as(x)
-
